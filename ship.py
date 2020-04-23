98d982177061139ef31410fa16453572a845dad8
karmajna <karmajna@users.noreply.github.com>
2020-04-23 11:57:26 -0500

    i pushed now I stage and will push again

--- ship.py
+++ ship.py
@@ -11,7 +11,7 @@ class Ship:
 
         self.screen_rect = ai_game.screen.get_rect()
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('/Users/mark/Documents/Python_Personal/PythonCrashCourse/alien_invasion/images/ship.bmp')
+        self.image = pygame.image.load('ship.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen. INTERESTING LINE 
